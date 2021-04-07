e2a02163df79b4b18d6affdce1919010b5ee0bda
Fefilova Anastasia <allblue312@gmail.com>
2021-04-07 13:21:21 +0300

    added acoustics balance and bass booster

--- src/js/filter.js
+++ src/js/filter.js
@@ -1,12 +1,12 @@
-function generateRandomNumber(min, max) {
-    return Math.random() * (max - min) + min;
-};
+// function generateRandomNumber(min, max) {
+//     return Math.random() * (max - min) + min;
+// };
 
 generateRandomNumber();
 document.querySelector('.eq_selector').addEventListener('change', function () {
     const n = this.value;
     switch (n) {
-        case 'Баланс': {
+        case 'Normal': {
             const a = document.querySelector('.eq-wrap')
             a.childNodes.forEach(el => {
                 el.value = 0;
@@ -14,10 +14,15 @@ document.querySelector('.eq_selector').addEventListener('change', function () {
             })
             break;
         }
-        case 'Акустика': {
+        case 'Acoustics': {
             const a = document.querySelector('.eq-wrap')
             a.childNodes.forEach((el, i) => {
                 switch (i) {
+                    case 0: {
+                        el.value = 0
+                        el.dispatchEvent(new Event('change'));
+                        break;
+                    }
                     case 1: {
                         el.value = 1
                         el.dispatchEvent(new Event('change'));
@@ -25,6 +30,31 @@ document.querySelector('.eq_selector').addEventListener('change', function () {
                     }
                     case 2: {
                         el.value = 2
+                        el.dispatchEvent(new Event('change'));
+                        break;
+                    }
+                    case 3: {
+                        el.value = 0
+                        el.dispatchEvent(new Event('change'));
+                        break;
+                    }
+                    case 4: {
+                        el.value = 0
+                        el.dispatchEvent(new Event('change'));
+                        break;
+                    }
+                    case 5: {
+                        el.value = 0
+                        el.dispatchEvent(new Event('change'));
+                        break;
+                    }
+                    case 6: {
+                        el.value = 0
+                        el.dispatchEvent(new Event('change'));
+                        break;
+                    }
+                    case 7: {
+                        el.value = 0
                         el.dispatchEvent(new Event('change'));
                         break;
                     }
@@ -38,17 +68,77 @@ document.querySelector('.eq_selector').addEventListener('change', function () {
                         el.dispatchEvent(new Event('change'));
                         break;
                     }
+
                 }
-            })
-        }
-        
-        case 'Рандом': {
-            const a = document.querySelector('.eq-wrap')
-            a.childNodes.forEach(el => {
-                el.value = generateRandomNumber(-16, 16);
-                el.dispatchEvent(new Event('change'));
             })
             break;
         }
+        case 'FullBass': {
+            const a = document.querySelector('.eq-wrap')
+            a.childNodes.forEach((el, i) => {
+                switch (i) {
+                    case 0: {
+                        el.value = 10
+                        el.dispatchEvent(new Event('change'));
+                        break;
+                    }
+                    case 1: {
+                        el.value = 10
+                        el.dispatchEvent(new Event('change'));
+                        break;
+                    }
+                    case 2: {
+                        el.value = 7
+                        el.dispatchEvent(new Event('change'));
+                        break;
+                    }
+                    case 3: {
+                        el.value = 4
+                        el.dispatchEvent(new Event('change'));
+                        break;
+                    }
+                    case 4: {
+                        el.value = 2
+                        el.dispatchEvent(new Event('change'));
+                        break;
+                    }
+                    case 5: {
+                        el.value = 2
+                        el.dispatchEvent(new Event('change'));
+                        break;
+                    }
+                    case 6: {
+                        el.value = -2
+                        el.dispatchEvent(new Event('change'));
+                        break;
+                    }
+                    case 7: {
+                        el.value = -4
+                        el.dispatchEvent(new Event('change'));
+                        break;
+                    }
+                    case 8: {
+                        el.value = -7
+                        el.dispatchEvent(new Event('change'));
+                        break;
+                    }
+                    case 9: {
+                        el.value = -10
+                        el.dispatchEvent(new Event('change'));
+                        break;
+                    }
+                }
+            })
+            break;
+        }
+
+        // case 'Рандом': {
+        //     const a = document.querySelector('.eq-wrap')
+        //     a.childNodes.forEach(el => {
+        //         el.value = generateRandomNumber(-16, 16);
+        //         el.dispatchEvent(new Event('change'));
+        //     })
+        //     break;
+        // }
     }
 })
